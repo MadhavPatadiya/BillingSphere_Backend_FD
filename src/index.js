662e22df830041b415f5f5c43ed8f0edafbf84bf
gosplex <johngospel003@gmail.com>
2024-04-08 13:46:05 +0530

    Installed mongodb on AWS IP

--- src/index.js
+++ src/index.js
@@ -31,9 +31,7 @@ const cred = {
 
 mongoose
   .connect(
-    // "mongodb+srv://johngospel003:LlJ6bdJ35zCzc53O@cluster0.efot3tr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    "mongodb+srv://billingspherefuerte:VhjtujqeZDbYvn6o@billingsphere.sg7iac6.mongodb.net/billingSphere?retryWrites=true&w=majority"
-
+    "mongodb://13.60.61.193:27017/BillingSphere2"
   )
   .then(() => {
     console.log("Connected to database");
